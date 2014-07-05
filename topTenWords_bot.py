3528ae3dcef93ace6ea28199b2775199e3a996e5
Swingtheory <tdietert@mail.usf.edu>
2014-07-05 11:59:48 -0400

    Changed OOP Implementation
    made the code a bit cleaner thank to another redditor's suggestions.

--- topTenWords_bot.py
+++ topTenWords_bot.py
@@ -1,5 +1,16 @@
+
 from bs4 import BeautifulSoup
 from functools import *
+from itertools import *
+
+import collections
+import operator
+import os
+import praw
+import requests
+from bs4 import BeautifulSoup
+from functools import *
+from itertools import *
 
 import collections
 import operator
@@ -11,23 +22,41 @@ import time
 
 class TenWordsBot():
 
-	def __init__(self, redditor, comment):
+	def __init__(self, current_thread):
+		self.r = praw.Reddit('this is u/swingtheorys user comment scraper!')
+		self.r.login('tenwords_bot', 'ilikepotatoesjug')
+		self.submission = self.r.get_submission(submission_id=current_thread)
 
-		self.redditor = redditor
+		self.responded_to = set()
 		self.ignore = self.get_ignore_list()
-		self.top_ten_words = self.get_top_ten()
-		self.comment = comment
 
-	def get_top_ten(self):
-		usr_comments = self.get_user_comments()
+	def moniter_thread(self):
+		while True:
+			for comment, redditor in zip(*self.get_commenters()):
+				if redditor.name not in self.responded_to and not self.already_replied(comment):
+					if comment.body[:15] == 'get_top_ten(me)':	
+						top_ten_words = self.get_top_ten(redditor)
+						self.reply_results(top_ten_words, comment)
+						self.responded_to.add(redditor.name)
+
+			time.sleep(30)
+
+	def get_commenters(self):
+		sub_comment_list = praw.helpers.flatten_tree(self.submission.comments)
+		commenters_list = reduce(lambda y, x: y + [x] if x.is_root else y, sub_comment_list, [])
+		redditor_list = reduce(lambda y, x: y + [x.author] if x.is_root else y, sub_comment_list, [])
+
+		return (commenters_list, redditor_list)
+
+	def get_top_ten(self, redditor):
+		usr_comments = self.get_user_comments(redditor)
 		word_list = self.collect_word_data(usr_comments)
 		word_dict = self.get_ordered_word_dict(word_list)
+		
 		return (collections.Counter(word_dict)).most_common(10)
 
-	def get_user_comments(self):
-
-		user_comments = self.redditor.get_comments(limit=None)
-
+	def get_user_comments(self, redditor):
+		user_comments = redditor.get_comments(limit=None)
 		comment_ids = set()
 		comment_list = []
 		for comment in user_comments:
@@ -60,6 +89,15 @@ class TenWordsBot():
 
 		return words_dict
 
+	def get_ordered_word_dict(self, words):
+		# returns the list of words in descending from most->least usage
+		return collections.OrderedDict(sorted(words.items(), key=lambda x: x[1], reverse=True))
+
+	def reply_results(self, top_ten, comment):
+		message = '\n\n'.join((word[0] + ' : ' + str(word[1]) for word in top_ten))
+		message = 'Your top ten words used:\n\n[ word : frequency ]\n\n' + message
+		comment.reply(message)
+
 	def filter_word(self, word):
 		if word in self.ignore:
 			return None
@@ -67,50 +105,16 @@ class TenWordsBot():
 			return None
 		if len(word) < 4:
 			return None
+
 		return word.lower()
 
-	def get_ordered_word_dict(self, words):
-		# returns the list of words in descending from most->least usage
-		return collections.OrderedDict(sorted(words.items(), key=lambda x: x[1], reverse=True))
+	def already_replied(self, comment):
+		for comment in comment.replies:
+			if comment.author.name == 'tenwords_bot':
+				return True
 
-	def reply_results(self):
-		message = '\n\n'.join((wordcount[0] + ' : ' + str(wordcount[1]) for wordcount in self.top_ten_words))
-		message = 'Your top ten words used:\n\n[ word : frequency ]\n\n' + message
-		self.comment.reply(message)
-
-def replied(comment):
-	for comment in comment.replies:
-		if comment.author.name == 'tenwords_bot':
-			return True
-	return False
+		return False
 
 if __name__ == '__main__':
-
-	r = praw.Reddit('this is u/swingtheorys user comment scraper!')
-	r.login('', '')
-
-	already_commented = set()
-	users_replied_to = set()
-
-	while True:
-		submission = r.get_submission(submission_id='')
-		sub_comment_list = praw.helpers.flatten_tree(submission.comments)
-
-		for comment in sub_comment_list:
-			print(comment.body)
-			# only replies to root comments
-			if comment.is_root and comment.body[:15] == 'get_top_ten(me)':
-
-				if not replied(comment):
-					# this looks messy, but perhaps necessary, read it like a sentence
-					if comment.author and comment.id not in already_commented and comment.author.name not in users_replied_to:
-
-						reddit_bot = TenWordsBot(comment.author, comment)
-						reddit_bot.reply_results()
-
-						already_commented.add(comment.id)
-						users_replied_to.add(comment.author.name)
-
-		time.sleep(600)
-
-
+	bot = TenWordsBot('29ur58')
+	bot.moniter_thread()
